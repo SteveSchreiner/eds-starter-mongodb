ae638313b557a8b07de4f19dc84ebf0d9b84e8ba
Ralph Schaer <ralphschaer@gmail.com>
2015-03-04 17:42:12 +0100

    Upgrade to spring boot 1.2.2 and remove custom compression code. This is now built in spring boot

--- src/main/java/ch/rasc/eds/starter/Application.java
+++ src/main/java/ch/rasc/eds/starter/Application.java
@@ -3,21 +3,16 @@ package ch.rasc.eds.starter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
 import org.springframework.util.StreamUtils;
 
 import ch.ralscha.extdirectspring.ExtDirectSpring;
@@ -63,21 +58,4 @@ public class Application extends SpringBootServletInitializer {
 		};
 	}
 
-	@Bean
-	@Profile("compression")
-	public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
-		return servletContainer -> ((TomcatEmbeddedServletContainerFactory) servletContainer)
-				.addConnectorCustomizers(connector -> {
-					AbstractHttp11Protocol<?> httpProtocol = (AbstractHttp11Protocol<?>) connector
-							.getProtocolHandler();
-					httpProtocol.setCompression("on");
-					httpProtocol.setCompressionMinSize(512);
-					String mimeTypes = httpProtocol.getCompressableMimeTypes();
-					String additionalMimeTypes = mimeTypes + ","
-							+ MediaType.APPLICATION_JSON_VALUE + ","
-							+ "application/javascript,text/css";
-
-					httpProtocol.setCompressableMimeTypes(additionalMimeTypes);
-				});
-	}
 }
